906696beb53ffb4bfe35e91d166909179c42c19c
ramayaser66 <ramayaser66@gmail.com>
2020-12-15 17:46:33 +0200

    add

---
+++ js/add-content.js
@@ -0,0 +1,1 @@
+vartoday=newDate();varhourNow=today.getHours();vargreeting;if(hourNow>18){greeting='Goodevening,Class!';}elseif(hourNow>12){greeting='Goodafternoon,Class!';}elseif(hourNow>=0){greeting='Goodmorning!';}else{greeting='Somethingwentwrong!';}document.write('<h3>'+greeting+'</h3>');
